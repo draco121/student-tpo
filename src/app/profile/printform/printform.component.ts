eff8e0605136c45134762277f069926dae674b8c
Rajeev Chourey <37259754+draco121@users.noreply.github.com>
2020-10-27 09:43:31 +0530

    print form activated

--- src/app/profile/printform/printform.component.ts
+++ src/app/profile/printform/printform.component.ts
@@ -20,17 +20,18 @@ export class PrintformComponent implements OnInit {
 
   print(){
       let data = document.getElementById('form')
-      html2canvas(data).then(canvas => {
+      let pdf = new jsPDF('p','mm','a4');
+      var width = pdf.internal.pageSize.getWidth();
+      var height = pdf.internal.pageSize.getHeight();
+      html2canvas(data,{
+        useCORS:true,
+        allowTaint:true
+      }).then(canvas => {
         // Few necessary setting options
-        var imgWidth = 208;
-        var pageHeight = 295;
-        var imgHeight = canvas.height * imgWidth / canvas.width;
-        var heightLeft = imgHeight;
 
         const contentDataURL = canvas.toDataURL('image/png')
         let pdf =  new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
-        var position = 0;
-        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+        pdf.addImage(contentDataURL, 'PNG', 0, 0,width,height)
         pdf.save('new-file.pdf'); // Generated PDF
         });
   }
